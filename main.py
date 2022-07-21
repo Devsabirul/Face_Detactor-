2821f911c07b304fce41787b6d9e8f6fefc290a7
Devsabirul <mdsabirulislam8089@gmail.com>
2022-07-21 13:38:22 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+
+cam = cv2.VideoCapture(0)
+detector = FaceDetector()
+
+
+while True:
+    success, img = cam.read()
+    img,bBoxes = detector.findFaces(img)
+    cv2.imshow("test", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
